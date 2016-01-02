90523ef5569ac4f07668760ad6ff842d6167e1e2
Antoine Augusti <antoine.augusti@gmail.com>
2016-01-02 11:00:14 +0100

    Bind port 8161 of the container to 8160 of the host

--- deployBroker.sh
+++ deployBroker.sh
@@ -7,7 +7,7 @@ docker run --name='activemq' -d \
 -e 'ACTIVEMQ_JMX_LOGIN=jmx_login' -e 'ACTIVEMQ_JMX_PASSWORD=jmx_password' \
 -e 'ACTIVEMQ_STATIC_TOPICS=topic1;topic2;topic3' \
 -e 'ACTIVEMQ_STATIC_QUEUES=queue1;queue2;queue3' \
--p 8161:8161 \
+-p 8160:8161 \
 -p 61616:61616 \
 -p 61613:61613 \
 -P webcenter/activemq:latest
